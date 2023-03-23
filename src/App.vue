de4c527c438c88f70f44ca1e9a523a3c193e05cd
Chukwuemeka Abuba <abubachukwuemeka@outlook.com>
2023-03-23 16:51:31 +0100

    creat harmbuger menu

--- src/App.vue
+++ src/App.vue
@@ -1,49 +1,66 @@
 <template>
-  <header class="mx-auto px-8 top-0 max-w-7xl">
-    <div class="flex justify-between items-center pt-4">
-      <router-link to="/"><img alt="logo" src="./assets/logo-bookmark.svg"></router-link>
+  <header :class="{'scrolled': scrollPosition}" class="mx-auto px-8 top-0 max-w-7xl">
+    <nav class="flex items-center justify-between flex-row py-5 w-full">
+      <div class="branding">
+        <img src="@/assets/logo-bookmark.svg" alt="logo">
+      </div>
 
-      <!-- <ul class="gap-14 flex items-center">
-        <li class="uppercase"><router-link to="/">Home</router-link></li>
-        <li class="uppercase"><router-link to="/about">About</router-link></li>
-        <router-link to="/counter"><button class="bg-[#42b983] uppercase text-white py-3 px-7 hover:opacity-75">Counter</button></router-link> 
-      </ul>  -->
-
-    <nav :class="{'block': menu, 'hidden': !menu}">
-      <ul>
-        <li class="uppercase"><router-link to="/">Home</router-link></li>
-        <li class="uppercase"><router-link to="/about">About</router-link></li>
-        <router-link to="/counter"><button class="bg-[#42b983] uppercase text-white py-3 px-7 hover:opacity-75">Counter</button></router-link>
+      <ul class="flex items-center flex-1 justify-end gap-5" v-show="!mobile">
+        <li><router-link to="/"><button class="uppercase tracking-widest hover:text-blue-400 lg:text-xs text-black font-bold">Home</button></router-link></li>
+        <li><router-link to="/about"><button class="uppercase tracking-widest hover:text-blue-400 lg:text-xs text-black font-bold">About</button></router-link></li>
+        <li><router-link to="/counter"><button class="lg:btn-red uppercase text-black lg:text-xs tracking-widest py-2 px-4 hover:opacity-75 
+        rounded shadow-lg font-bold border-2 hover:border-blue-400 lg:hover:bg-transparent lg:hover:text-slate-800 transition-all duration-200">Counter</button></router-link></li>
       </ul>
-    </nav>
 
-    <div>
-      <button>
-        <!-- <img 
-          class="md:hidden" 
-          @click="changeMenu" 
-          alt="" 
-          :src="menu ? 'setClose' : 'setHamBurger'"
-        > -->
-      </button>
-    </div>
-    </div>
+      <div>
+        <button @click="toggleMobileNav" v-show="mobile" :class="{'icon-active': mobileNav}">
+          <img :src="setHamburger" alt="hamburger logo">
+        </button>
+      </div>
+      <transition name="mobile-nav">
+        <ul class="flex flex-col items-center gap-4 fixed min-w-[350px] h-full bg-white top-11 right-0" v-show="mobileNav">
+          <li><router-link to="/"><button class="mt-8 uppercase tracking-widest hover:text-blue-400 lg:text-xs mr-0 text-black font-bold">Home</button></router-link></li>
+          <li><router-link to="/about"><button class="uppercase tracking-widest hover:text-blue-400 lg:text-xs mr-0 text-black font-bold">About</button></router-link></li>
+          <li><router-link to="/counter"><button class="lg:btn-red uppercase text-black lg:text-xs tracking-widest py-2 px-4 hover:opacity-75 
+          rounded shadow-lg font-bold border-2 hover:border-blue-400 lg:hover:bg-transparent lg:hover:text-slate-800 transition-all duration-200 mr-0">Counter</button></router-link></li>
+      </ul>
+      </transition>
+    </nav>
   </header>
   <router-view/>
 </template>
 
 <script>
-
+import hamburger from "./assets/icon-hamburger.svg"
 export default {
   data() {
     return {
-      menu: false,
-      
+      isMenuOpen: false,
+      setHamburger: hamburger,
+      scrollPosition: null,
+      mobile : true,
+      mobileNav: null,
+      windowWidth: null
     }
   },
+  created() {
+    window.addEventListener('resize', this.checkScreen())
+    this.checkScreen();
+  },
   methods: { 
-    changeMenu() {
-      this.menu = !this.menu
+    toggleMobileNav() {
+      this.mobileNav = !this.mobileNav
+    },
+
+    checkScreen() {
+      this.windowWidth = window.innerWidth;
+      if (this.windowWidth <= 750) {
+        this.mobile = true;
+        return;
+      }
+      this.mobile = false;
+      this.mobileNav = false;
+      return;
     }
   }
 }
@@ -54,7 +71,7 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
 }
 
-nav {
+/* nav {
   padding: 30px;
 }
 
@@ -65,5 +82,11 @@ nav a {
 
 nav a.router-link-exact-active {
   color: #42b983;
+} */
+
+header {
+  /* background-color: rgba(0, 0, 0, 0.8); */
+  z-index: 99;
+  transition: .5s ease all;
 }
 </style>
--- src/views/NotFound.vue
+++ src/views/NotFound.vue
@@ -1,5 +1,12 @@
 <template>
-    <h2>404</h2>
-    <h3>Page not found</h3>
+    <section class="flex flex-col justify-center items-center gap-2 mt-40">
+        <h2 class=" text-6xl font-bold md:text-8xl">404</h2>
+        <h3 class="font-semibold">Didn't find anything here</h3>
+        <div class="flex items-center gap-2">
+            <p>Go to</p> 
+            <router-link to="/counter"><button class="lg:text-xs text-blue-400 tracking-wide 
+            transition-all duration-200">Counter</button></router-link>
+        </div>
+    </section>
 </template>
 
